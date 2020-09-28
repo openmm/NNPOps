21b88b825a3e36923ecf08416a9bdf688a997812
Raimondas Galvelis <r.galvelis@acellera.com>
2020-09-28 15:50:18 +0200

    Wrap CpuANISymmetryFunctions as a custom Pytorch class

--- pytorch/SymmetryFunctions.cpp
+++ pytorch/SymmetryFunctions.cpp
@@ -24,6 +24,55 @@
 #include <torch/script.h>
 #include "CpuANISymmetryFunctions.h"
 
+class CustomCpuANISymmetryFunctions : public torch::CustomClassHolder {
+public:
+    CustomCpuANISymmetryFunctions(int64_t numSpecies_,
+                                  double Rcr,
+                                  double Rca,
+                                  const std::vector<double>& EtaR,
+                                  const std::vector<double>& ShfR,
+                                  const std::vector<double>& EtaA,
+                                  const std::vector<double>& Zeta,
+                                  const std::vector<double>& ShfA,
+                                  const std::vector<double>& ShfZ,
+                                  const std::vector<int64_t>& atomSpecies_) : torch::CustomClassHolder() {
+
+        numAtoms = atomSpecies_.size();
+        numSpecies = numSpecies_;
+        const std::vector<int> atomSpecies(atomSpecies_.begin(), atomSpecies_.end());
+
+        for (const float eta: EtaR)
+            for (const float rs: ShfR)
+                radialFunctions.push_back({eta, rs});
+
+        for (const float eta: EtaA)
+            for (const float zeta: Zeta)
+                for (const float rs: ShfA)
+                    for (const float thetas: ShfZ)
+                        angularFunctions.push_back({eta, rs, zeta, thetas});
+
+        symFunc = std::make_shared<CpuANISymmetryFunctions>(numAtoms, numSpecies, Rcr, Rca, false, atomSpecies, radialFunctions, angularFunctions, true);
+    };
+
+    torch::autograd::tensor_list forward(const torch::Tensor& positions_) {
+
+        const auto positions = positions_.toType(torch::kFloat);
+        auto radial  = torch::empty({numAtoms, numSpecies * (int)radialFunctions.size()}, torch::kFloat);
+        auto angular = torch::empty({numAtoms, numSpecies * (numSpecies + 1) / 2 * (int)angularFunctions.size()}, torch::kFloat);
+
+        symFunc->computeSymmetryFunctions(positions.data_ptr<float>(), nullptr, radial.data_ptr<float>(), angular.data_ptr<float>());
+
+        return {radial, angular};
+    };
+
+private:
+    int numAtoms;
+    int numSpecies;
+    std::vector<RadialFunction> radialFunctions;
+    std::vector<AngularFunction> angularFunctions;
+    std::shared_ptr<CpuANISymmetryFunctions> symFunc;
+};
+
 class GradANISymmetryFunction : public torch::autograd::Function<GradANISymmetryFunction> {
 
 public:
@@ -40,36 +89,18 @@ public:
                                                 const std::vector<int64_t>& atomSpecies_,
                                                 const torch::Tensor& positions_) {
 
-        const int numAtoms = atomSpecies_.size();
-        const std::vector<int> atomSpecies(atomSpecies_.begin(), atomSpecies_.end());
+        const auto symFunc = torch::make_custom_class<CustomCpuANISymmetryFunctions>(numSpecies, Rcr, Rca, EtaR, ShfR, EtaA, Zeta, ShfA, ShfZ, atomSpecies_);
+        ctx->saved_data["symFunc"] = symFunc;
 
-        std::vector<RadialFunction> radialFunctions;
-        for (const float eta: EtaR)
-            for (const float rs: ShfR)
-                radialFunctions.push_back({eta, rs});
-
-        std::vector<AngularFunction> angularFunctions;
-        for (const float eta: EtaA)
-            for (const float zeta: Zeta)
-                for (const float rs: ShfA)
-                    for (const float thetas: ShfZ)
-                        angularFunctions.push_back({eta, rs, zeta, thetas});
-
-        CpuANISymmetryFunctions sf(numAtoms, numSpecies, Rcr, Rca, false, atomSpecies, radialFunctions, angularFunctions, true);
-
-        const auto positions = positions_.toType(torch::kFloat);
-        auto radial  = torch::empty({numAtoms, numSpecies * (int)radialFunctions.size()}, torch::kFloat);
-        auto angular = torch::empty({numAtoms, numSpecies * (numSpecies + 1) / 2 * (int)angularFunctions.size()}, torch::kFloat);
-
-        sf.computeSymmetryFunctions(positions.data_ptr<float>(), nullptr, radial.data_ptr<float>(), angular.data_ptr<float>());
-
-        return {radial, angular};
+        return symFunc.toCustomClass<CustomCpuANISymmetryFunctions>()->forward(positions_);
     };
 
     static torch::autograd::tensor_list backward(torch::autograd::AutogradContext *ctx, torch::autograd::tensor_list grads) {
 
         const auto& radialGrad = grads[0];
         const auto& angularGrad = grads[1];
+
+        const auto symFunc = ctx->saved_data["symFunc"].toCustomClass<CustomCpuANISymmetryFunctions>();
 
         // compute the gradients
 
@@ -104,5 +135,17 @@ static torch::autograd::tensor_list ANISymmetryFunction(int64_t numSpecies,
 }
 
 TORCH_LIBRARY(NNPOps, m) {
+    m.class_<CustomCpuANISymmetryFunctions>("CustomCpuANISymmetryFunctions")
+        .def(torch::init<int64_t,                        // numSpecies
+                         double,                         // Rcr
+                         double,                         // Rca
+                         const std::vector<double>&,     // EtaR
+                         const std::vector<double>&,     // ShfR
+                         const std::vector<double>&,     // EtaA
+                         const std::vector<double>&,     // Zeta
+                         const std::vector<double>&,     // ShfA
+                         const std::vector<double>&,     // ShfZ
+                         const std::vector<int64_t>&>()) // atomSpecies
+        .def("forward", &CustomCpuANISymmetryFunctions::forward);
     m.def("ANISymmetryFunction", ANISymmetryFunction);
 }
