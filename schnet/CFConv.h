269a8b405c608c46048fc8f6a2e868c5e49ae933
Peter Eastman <peastman@stanford.edu>
2020-11-06 14:52:42 -0800

    Added more documentation

--- schnet/CFConv.h
+++ schnet/CFConv.h
@@ -24,12 +24,36 @@
  * SOFTWARE.
  */
 
+/**
+ * This class represents a neighbor list for use in computing CFConv layers.
+ * A single object can be used for all layers in the models.  Create it at the
+ * same time you create the model, call build() every time the atom positions
+ * change, and pass it to the methods of CFConv objects that do computations
+ * based on positions.
+ * 
+ * This is an abstract class.  Subclasses provide implementations on particular
+ * types of hardware.
+ */
 class CFConvNeighbors {
 public:
+    /**
+     * Create an object for computing neighbor lists.
+     * 
+     * @param numAtoms    the number of atoms in the system being modeled
+     * @param cutoff      the cutoff distance
+     * @param periodic    whether to use periodic boundary conditions.
+     */
     CFConvNeighbors(int numAtoms, float cutoff, bool periodic) : numAtoms(numAtoms), cutoff(cutoff), periodic(periodic) {
     }
     virtual ~CFConvNeighbors() {
     }
+    /**
+     * Rebuild the neighbor list based on a new set of positions.
+     * 
+     * @param positions           an array of shape [numAtoms][3] containing the position of each atom
+     * @param periodicBoxVectors  an array of shape [3][3] containing the periodic box vectors.  If periodic boundary conditions are
+     *                            not used, this is ignored and may be NULL.
+     */
     virtual void build(const float* positions, const float* periodicBoxVectors) = 0;
     /**
      * Get whether the periodic box vectors specified in the most recent call to build() described
@@ -60,14 +84,37 @@ private:
     bool periodic;
 };
 
+/**
+ * This class computes the continuous filter convolution (cfconv) function used in SchNet.
+ * For every pair of atoms, it performs the following calculations:
+ * 
+ * 1. Compute a set of Gaussian basis functions describing the distance between them.
+ * 2. Pass them through a dense layer.
+ * 3. Apply a shifted softplus activation function.
+ * 4. Pass the result through a second dense layer.
+ * 5. Apply a cosine cutoff function to make interactions smoothly go to zero at the cutoff.
+ * 
+ * For each atom, the output is the sum over all neighbors of the above calculation multiplied
+ * by the neighbor's input vector.
+ * 
+ * This calculation is designed to match the behavior of SchNetPack.  It is similar but not
+ * identical to that described in the original SchNet publication.
+ * 
+ * Create an instance of this class at the same time you create the model and then reuse it for every
+ * calculation on that model.  You also can share a single object between multiple layers if the
+ * hyperparameters specified in the constructor are the same for all of them.
+ */
 class CFConv {
 public:
     /**
      * Construct on object for computing continuous filter convolution (cfconv) functions.
      *
-     * @param numAtoms            the number of atoms in the system
-     * @param cutoff              the cutoff distance
-     * @param periodic            whether to apply periodic boundary conditions
+     * @param numAtoms       the number of atoms in the system
+     * @param width          the number of elements in the input and output vectors
+     * @param numGaussians   the number of Gaussian basis functions to use, uniformly spaced between 0 and cutoff
+     * @param cutoff         the cutoff distance
+     * @param periodic       whether to apply periodic boundary conditions
+     * @param gaussianWidth  the width of the Gaussian basis functions
      */
     CFConv(int numAtoms, int width, int numGaussians, float cutoff, bool periodic, float gaussianWidth) :
            numAtoms(numAtoms), width(width), numGaussians(numGaussians), cutoff(cutoff), periodic(periodic), gaussianWidth(gaussianWidth) {
@@ -77,9 +124,17 @@ public:
     /**
      * Compute the output of the layer.
      *
+     * @param neighbors           a neighbor list for accelerating the calculation.  You must have already called
+     *                            build() on the neighbor list with the same positions and box vectors.
      * @param positions           an array of shape [numAtoms][3] containing the positions of each atom
      * @param periodicBoxVectors  an array of shape [3][3] containing the periodic box vectors.  If periodic boundary conditions are
      *                            not used, this is ignored and may be NULL.
+     * @param input               an array of shape [numAtoms][width] containing the input vectors
+     * @param output              an array of shape [numAtoms][width] to store the output vectors into
+     * @param w1                  an array of shape [numGaussians][width] containing the weights of the first dense layer
+     * @param b1                  an array of length [width] containing the biases of the first dense layer
+     * @param w2                  an array of shape [width][width] containing the weights of the second dense layer
+     * @param b2                  an array of length [width] containing the biases of the second dense layer
      */
     virtual void compute(const CFConvNeighbors& neighbors, const float* positions, const float* periodicBoxVectors,
                          const float* input, float* output, const float* w1, const float* b1, const float* w2, const float* b2) = 0;
@@ -87,12 +142,19 @@ public:
      * Given the derivatives of some function E (typically energy) with respect to the outputs, backpropagate them
      * to find the derivates of E with respect to the inputs and atom positions.
      *
-     * This must be called after compute().  It uses the atom positions and box vectors that were specified in the most
-     * recent call to that function.
-     *
-     * @param outputDeriv      an array of shape [numAtoms][outputWidth] containing the derivative of E with respect to each output value
-     * @param inputDeriv       an array of shape [numAtoms][inputWidth] to store the derivative of E with respect to each input value into
-     * @param positionDeriv    an array of shape [numAtoms][3] to store the derivative of E with respect to the atom positions into
+     * @param neighbors           a neighbor list for accelerating the calculation.  You must have already called
+     *                            build() on the neighbor list with the same positions and box vectors.
+     * @param positions           an array of shape [numAtoms][3] containing the positions of each atom
+     * @param periodicBoxVectors  an array of shape [3][3] containing the periodic box vectors.  If periodic boundary conditions are
+     *                            not used, this is ignored and may be NULL.
+     * @param input               an array of shape [numAtoms][width] containing the input vectors
+     * @param outputDeriv         an array of shape [numAtoms][width] containing the derivative of E with respect to each output value
+     * @param inputDeriv          an array of shape [numAtoms][width] to store the derivative of E with respect to each input value into
+     * @param positionDeriv       an array of shape [numAtoms][3] to store the derivative of E with respect to the atom positions into
+     * @param w1                  an array of shape [numGaussians][width] containing the weights of the first dense layer
+     * @param b1                  an array of length [width] containing the biases of the first dense layer
+     * @param w2                  an array of shape [width][width] containing the weights of the second dense layer
+     * @param b2                  an array of length [width] containing the biases of the second dense layer
      */
     virtual void backprop(const CFConvNeighbors& neighbors, const float* positions, const float* periodicBoxVectors,
                           const float* input, const float* outputDeriv, float* inputDeriv, float* positionDeriv,
@@ -103,9 +165,15 @@ public:
     int getNumAtoms() const {
         return numAtoms;
     }
+    /**
+     * Get the number of elements in the input and output vectors.
+     */
     int getWidth() const {
         return width;
     }
+    /**
+     * Get the number of Gaussian basis functions.
+     */
     int getNumGaussians() const {
         return numGaussians;
     }
@@ -121,6 +189,9 @@ public:
     bool getPeriodic() const {
         return periodic;
     }
+    /**
+     * Get the width of the Gaussian basis functions.
+     */
     float getGaussianWidth() const {
         return gaussianWidth;
     }
