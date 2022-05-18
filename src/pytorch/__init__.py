98381945d09640e88a7911c0756b13dbf88cbd0c
Raimondas Galvelis <r.galvelis@acellera.com>
2022-05-18 20:37:54 +0200

    Refatoring for documentation

--- src/pytorch/__init__.py
+++ src/pytorch/__init__.py
@@ -1,4 +1,15 @@
-from NNPOps.OptimizedTorchANI import OptimizedTorchANI
-import torch
+'''
+High-performance PyTorch operations for neural network potentials
+'''
 
-getNeighborPairs = torch.ops.neighbors.getNeighborPairs
+from NNPOps.OptimizedTorchANI import OptimizedTorchANI
+from torch import ops, Tensor
+from typing import Tuple
+
+
+def getNeighborPairs(positions: Tensor, cutoff: float, max_num_neighbors: int) -> Tuple[Tensor, Tensor]:
+    '''
+    TODO
+    '''
+
+    return ops.neighbors.getNeighborPairs(positions, cutoff, max_num_neighbors)
