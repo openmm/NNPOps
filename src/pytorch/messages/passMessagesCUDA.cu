b9de467efadd01b3173250c90a68a17a7fc29bd4
Raimondas Galvelis <r.galvelis@acellera.com>
2022-06-07 18:21:50 +0200

    Support pre-Kepler GPUs

--- src/pytorch/messages/passMessagesCUDA.cu
+++ src/pytorch/messages/passMessagesCUDA.cu
@@ -21,6 +21,21 @@ inline Accessor<scalar_t, num_dims> get_accessor(const Tensor& tensor) {
     return tensor.packed_accessor32<scalar_t, num_dims, RestrictPtrTraits>();
 };
 
+// Support pre-Kepler GPUs
+#if defined(__CUDA_ARCH__) && __CUDA_ARCH__ < 600
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                __double_as_longlong(val + __longlong_as_double(assumed)));
+    } while (assumed != old);
+    return __longlong_as_double(old);
+}
+#endif
+
 template <typename scalar_t> __global__ void kernel_forward(
     const Accessor<int32_t, 2> neighbors,
     const Accessor<scalar_t, 2> messages,
