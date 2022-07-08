c48fb0b520cae6ef78f102d83fe90107cc397f23
Raimondas Galvelis <r.galvelis@acellera.com>
2022-07-08 12:36:40 +0200

    Fix an autograd loop in CPU

--- src/pytorch/neighbors/getNeighborPairsCPU.cpp
+++ src/pytorch/neighbors/getNeighborPairsCPU.cpp
@@ -44,6 +44,7 @@ static tuple<Tensor, Tensor, Tensor> forward(const Tensor& positions,
     if (max_num_neighbors_ == -1) {
         const Tensor mask = distances > cutoff;
         neighbors.index_put_({Slice(), mask}, -1);
+        deltas = deltas.clone(); // Brake an autograd loop
         deltas.index_put_({mask, Slice()}, NAN);
         distances.index_put_({mask}, NAN);
 
