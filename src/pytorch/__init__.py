d6eb763775ba172e9fa2a13fb96668c14bc84f0f
RaulPPealez <raulppelaez@gmail.com>
2023-03-09 10:28:32 +0100

    Update again how libNNPOpsPyTorch.so is found in __init__.py

--- src/pytorch/__init__.py
+++ src/pytorch/__init__.py
@@ -3,8 +3,16 @@ High-performance PyTorch operations for neural network potentials
 '''
 import os.path
 import torch
+import site
 
-torch.ops.load_library(os.path.join(os.path.dirname(__file__), 'libNNPOpsPyTorch.so'))
-torch.classes.load_library(os.path.join(os.path.dirname(__file__), 'libNNPOpsPyTorch.so'))
+# look for NNPOps/libNNPOpsPyTorch.so in all the paths returned by site.getsitepackages()
+for path in site.getsitepackages():
+    if os.path.exists(os.path.join(path, 'NNPOps/libNNPOpsPyTorch.so')):
+        torch.ops.load_library(os.path.join(path, 'NNPOps/libNNPOpsPyTorch.so'))
+        break
+else:
+    # if we didn't find it, look for NNPOps/libNNPOpsPyTorch.so in the same directory as this file
+    torch.ops.load_library(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'libNNPOpsPyTorch.so'))
+
 
 from NNPOps.OptimizedTorchANI import OptimizedTorchANI
