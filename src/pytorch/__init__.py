4fb4b2e6d22bdb8c15b2c59cbaa117576a2b6393
RaulPPealez <raulppelaez@gmail.com>
2023-03-15 10:21:10 +0100

    Use relative path to load NNPOps library in __init__.py

--- src/pytorch/__init__.py
+++ src/pytorch/__init__.py
@@ -3,16 +3,8 @@ High-performance PyTorch operations for neural network potentials
 '''
 import os.path
 import torch
-import site
 
-# look for NNPOps/libNNPOpsPyTorch.so in all the paths returned by site.getsitepackages()
-for path in site.getsitepackages():
-    if os.path.exists(os.path.join(path, 'NNPOps/libNNPOpsPyTorch.so')):
-        torch.ops.load_library(os.path.join(path, 'NNPOps/libNNPOpsPyTorch.so'))
-        break
-else:
-    # if we didn't find it, look for NNPOps/libNNPOpsPyTorch.so in the same directory as this file
-    torch.ops.load_library(os.path.join(os.path.dirname(__file__), 'libNNPOpsPyTorch.so'))
+torch.ops.load_library(os.path.join(os.path.dirname(__file__), 'libNNPOpsPyTorch.so'))
 
 
 from NNPOps.OptimizedTorchANI import OptimizedTorchANI
