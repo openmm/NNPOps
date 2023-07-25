be7796eff036826baf26491f1ba4d916be955438
RaulPPealez <raulppelaez@gmail.com>
2023-07-25 13:07:45 +0200

    Make some tests use deterministic torch algorithms

--- src/pytorch/TestBatchedNN.py
+++ src/pytorch/TestBatchedNN.py
@@ -34,40 +34,52 @@ def test_import():
     import NNPOps
     import NNPOps.BatchedNN
 
+class DeterministicTorch:
+    def __enter__(self):
+        if torch.are_deterministic_algorithms_enabled():
+            self._already_enabled = True
+            return
+        self._already_enabled = False
+        torch.use_deterministic_algorithms(True)
+
+    def __exit__(self, type, value, traceback):
+        if not self._already_enabled:
+            torch.use_deterministic_algorithms(False)
+
 @pytest.mark.parametrize('deviceString', ['cpu', 'cuda'])
 @pytest.mark.parametrize('molFile', ['1hvj', '1hvk', '2iuz', '3hkw', '3hky', '3lka', '3o99'])
 def test_compare_with_native(deviceString, molFile):
 
     if deviceString == 'cuda' and not torch.cuda.is_available():
         pytest.skip('CUDA is not available')
+    with DeterministicTorch():
+        from NNPOps.BatchedNN import TorchANIBatchedNN
 
-    from NNPOps.BatchedNN import TorchANIBatchedNN
+        device = torch.device(deviceString)
 
-    device = torch.device(deviceString)
+        mol = mdtraj.load(os.path.join(molecules, f'{molFile}_ligand.mol2'))
+        atomicNumbers = torch.tensor([[atom.element.atomic_number for atom in mol.top.atoms]], device=device)
+        atomicPositions = torch.tensor(mol.xyz, dtype=torch.float32, requires_grad=True, device=device)
 
-    mol = mdtraj.load(os.path.join(molecules, f'{molFile}_ligand.mol2'))
-    atomicNumbers = torch.tensor([[atom.element.atomic_number for atom in mol.top.atoms]], device=device)
-    atomicPositions = torch.tensor(mol.xyz, dtype=torch.float32, requires_grad=True, device=device)
+        nnp = torchani.models.ANI2x(periodic_table_index=True).to(device)
+        energy_ref = nnp((atomicNumbers, atomicPositions)).energies
+        energy_ref.backward()
+        grad_ref = atomicPositions.grad.clone()
 
-    nnp = torchani.models.ANI2x(periodic_table_index=True).to(device)
-    energy_ref = nnp((atomicNumbers, atomicPositions)).energies
-    energy_ref.backward()
-    grad_ref = atomicPositions.grad.clone()
+        nnp.neural_networks = TorchANIBatchedNN(nnp.species_converter, nnp.neural_networks, atomicNumbers).to(device)
+        energy = nnp((atomicNumbers, atomicPositions)).energies
+        atomicPositions.grad.zero_()
+        energy.backward()
+        grad = atomicPositions.grad.clone()
 
-    nnp.neural_networks = TorchANIBatchedNN(nnp.species_converter, nnp.neural_networks, atomicNumbers).to(device)
-    energy = nnp((atomicNumbers, atomicPositions)).energies
-    atomicPositions.grad.zero_()
-    energy.backward()
-    grad = atomicPositions.grad.clone()
+        energy_error = torch.abs((energy - energy_ref)/energy_ref)
+        grad_error = torch.max(torch.abs((grad - grad_ref)/grad_ref))
 
-    energy_error = torch.abs((energy - energy_ref)/energy_ref)
-    grad_error = torch.max(torch.abs((grad - grad_ref)/grad_ref))
-
-    assert energy_error < 5e-7
-    if molFile == '3o99':
-        assert grad_error < 0.025 # Some numerical instability
-    else:
-        assert grad_error < 5e-3
+        assert energy_error < 5e-7
+        if molFile == '3o99':
+            assert grad_error < 0.025 # Some numerical instability
+        else:
+            assert grad_error < 5e-3
 
 @pytest.mark.parametrize('deviceString', ['cpu', 'cuda'])
 @pytest.mark.parametrize('molFile', ['1hvj', '1hvk', '2iuz', '3hkw', '3hky', '3lka', '3o99'])
