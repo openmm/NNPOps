675754c82e0c0fac9c917fd7786790acfa1c43e6
SimonBoothroyd <simon.boothroyd@hotmail.com>
2023-10-14 08:44:04 -0400

    Clean-up prefix

--- src/pytorch/__init__.py
+++ src/pytorch/__init__.py
@@ -3,10 +3,12 @@ High-performance PyTorch operations for neural network potentials
 '''
 import os.path
 import torch
-from torch.utils.cpp_extension import CLIB_PREFIX, IS_MACOS, LIB_EXT
+from torch.utils.cpp_extension import IS_WINDOWS, IS_MACOS, LIB_EXT
 
-_LIBRARY_EXT = LIB_EXT if not IS_MACOS else ".dylib"
-_LIBRARY_NAME = f'{CLIB_PREFIX}NNPOpsPyTorch{_LIBRARY_EXT}'
+# by default torch assumes OSX has a .so ext, so we need to override
+_LIBRARY_SUFFIX = LIB_EXT if not IS_MACOS else ".dylib"
+_LIBRARY_PREFIX = '' if IS_WINDOWS else "lib"
+_LIBRARY_NAME = f'{_LIBRARY_PREFIX}NNPOpsPyTorch{_LIBRARY_SUFFIX}'
 
 torch.ops.load_library(os.path.join(os.path.dirname(__file__), _LIBRARY_NAME))
 
