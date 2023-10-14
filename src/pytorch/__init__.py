83b07c9966e5fee147ab83ffca734a5b6da13d5a
SimonBoothroyd <simon.boothroyd@hotmail.com>
2023-10-14 08:22:46 -0400

    Add OSX support

--- src/pytorch/__init__.py
+++ src/pytorch/__init__.py
@@ -2,9 +2,15 @@
 High-performance PyTorch operations for neural network potentials
 '''
 import os.path
-import torch
+import sys
+import torch.utils.cpp_extension
 
-torch.ops.load_library(os.path.join(os.path.dirname(__file__), 'libNNPOpsPyTorch.so'))
+_LIBRARY_NAME = f'NNPOpsPyTorch.{torch.utils.cpp_extension}'
+
+if not sys.platform.lower().startswith("win"):
+    _LIBRARY_NAME = f'lib{_LIBRARY_NAME}'
+
+torch.ops.load_library(os.path.join(os.path.dirname(__file__), _LIBRARY_NAME))
 
 
 from NNPOps.OptimizedTorchANI import OptimizedTorchANI
